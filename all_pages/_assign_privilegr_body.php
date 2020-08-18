6a83b44d77b7214d9b66b272b60b67eaaad4a741
pang <kemchira.pangk@mail.kmutt.ac.th>
2020-08-18 19:35:22 +0700

    Init Project

---
+++ all_pages/_assign_privilegr_body.php
@@ -0,0 +1,115 @@
+<div class="box box-info">
+        <div class="box-header with-border">
+          <h3 class="box-title">Assign Privilege</h3>
+
+          <div class="box-tools pull-right">
+            <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i></button>
+            <button type="button" class="btn btn-box-tool" data-widget="remove"><i class="fa fa-remove"></i></button>
+          </div>
+        </div>
+        <div class="box-body">
+          <div class="row">
+            <div class="col-md-6">
+              <div class="form-group">
+                <label>Document title</label>
+                    <input type="email" class="form-control" id="inputEmail3" placeholder="Enter Document title">
+              </div>
+            </div>
+
+            <div class="col-md-4">
+            <div class="form-group">
+                  <label>File input</label>
+                  <input type="text" class="form-control" placeholder="Document ID" disabled>
+                </div>
+            </div>
+          </div>
+        </div>
+
+        <div class="box-body">
+          <div class="row">
+            <div class="col-md-6">
+              <div class="form-group">
+                <label>Step number of approval</label>
+                <select class="form-control select2" style="width: 100%;">
+                  <option selected="selected">Choose Number</option>
+                  <option>1</option>
+                </select>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <!-- /.box-header -->
+        <div class="box-body">
+          <div class="row">
+
+            <div class="col-md-4">
+              <div class="form-group">
+                <label>Department</label>
+                <select class="form-control select2" style="width: 100%;">
+                  <option selected="selected">Choose Department</option>
+                  <option>1</option>
+                  <option>2</option>
+                  <option>3</option>
+                  <option>4</option>
+                  <option>5</option>
+                  <option>6</option>
+                </select>
+              </div>
+            </div>
+
+            <div class="col-md-4">
+              <div class="form-group">
+                <label>Sub Department</label>
+                <select class="form-control select2" style="width: 100%;">
+                  <option selected="selected">Choose Sub Department</option>
+                  <option>1</option>
+                  <option>2</option>
+                  <option>3</option>
+                  <option>4</option>
+                  <option>5</option>
+                  <option>6</option>
+                </select>
+              </div>
+            </div>
+
+            <div class="col-md-4">
+              <div class="form-group">
+                <label>Position</label>
+                <select class="form-control select2" style="width: 100%;">
+                  <option selected="selected">Choose Position</option>
+                  <option>1</option>
+                  <option>2</option>
+                  <option>3</option>
+                  <option>4</option>
+                  <option>5</option>
+                  <option>6</option>
+                </select>
+              </div>
+            </div>
+
+            <div class="col-md-6">
+              <div class="form-group">
+                <label>Name For Approve</label>
+                <select class="form-control select2" style="width: 100%;">
+                  <option selected="selected">Choose Sub Department</option>
+                  <option>1</option>
+                  <option>2</option>
+                  <option>3</option>
+                  <option>4</option>
+                  <option>5</option>
+                  <option>6</option>
+                </select>
+              </div>
+            </div>
+            
+          </div>
+        </div>
+        <div class="box-footer">
+          Please check the accuracy before pressing submit.
+        </div>
+        <div class="box-footer">
+                <button type="submit" class="btn btn-default"><a href="_createdocument.php">Back</a></button>
+                <button type="submit" class="btn btn-info pull-right">Submit</button>
+              </div>
+</div>
